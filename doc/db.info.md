2891d3536061a9177eb04f2b401d8f4ce3f7bdc1
zymon <szymon.lubieniecki@gmail.com>
2016-07-07 17:32:17 +0200

    HSQL DB + initial schema creation

---
+++ doc/db.info.md
@@ -0,0 +1,3 @@
+# DB config
+1. user: lotto_user
+2. password: ,9%U'd-cWjD>=+_Sev3ensH5<DhU^: 
--- src/test/java/com/esolutions/lotto/LottoApplicationTests.java
+++ src/test/java/com/esolutions/lotto/LottoApplicationTests.java
@@ -2,11 +2,11 @@ package com.esolutions.lotto;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LottoApplication.class)
 public class LottoApplicationTests {
 
 	@Test
